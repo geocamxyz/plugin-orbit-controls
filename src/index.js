4373625df80867df6d27edf7b813c2b45da2419c
Julian Cox <julian.cox@webspeed.co.nz>
2024-04-17 16:01:02 +1200

    change orbit-contols to web component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-export { orbitControls } from './lib/orbit-controls.js';
+import { orbitControls } from "./lib/orbit-controls.js";
+
+export class GeocamViewerOrbitControls extends HTMLElement {
+  constructor() {
+    super();
+    this.controls = null;
+    // this.yaw = this.getAttribute('yaw') || 0;
+    console.log("orbit-controls init");
+  }
+
+  connectedCallback() {
+    console.log("orbit-controls connected");
+    const node = this;
+    this.controls = new orbitControls();
+    debugger;
+    const parent = this.parentNode;
+    if (parent.viewer && parent.viewer.plugin) {
+      // Call a method on the parent
+      parent.viewer.plugin(this.controls);
+    } else {
+      console.error(
+        "GeocamViewerOrbitControls must be a child of GeocamViewer"
+      );
+    }
+  }
+
+  disconnectedCallback() {
+    this.controls = null;
+    console.log("orbit controls disconnected");
+    // Clean up the viewer
+  }
+}
+
+window.customElements.define(
+  "geocam-viewer-orbit-controls",
+  GeocamViewerOrbitControls
+);
