0ac85e3a8e08e1d79d8dfd4bf85c534783702320
Luke Reid <luke@reid.org.nz>
2025-11-04 15:26:12 +1300

    Guard plugin registration until geocam-viewer ready

--- src/index.js
+++ src/index.js
@@ -4,28 +4,42 @@ export class GeocamViewerOrbitControls extends HTMLElement {
   constructor() {
     super();
     this.controls = null;
+    this.viewer = null;
+    this.plugin = null;
     // this.yaw = this.getAttribute('yaw') || 0;
     console.log("orbit-controls init");
   }
 
   connectedCallback() {
     console.log("orbit-controls connected");
-    const node = this;
-    this.plugin = new orbitControls();
-    const parent = this.parentNode;
-    this.viewer = parent.viewer;
-    if ( this.viewer &&  this.viewer.plugin) {
-      // Call a method on the parent
-        this.viewer.plugin(this.plugin);
-    } else {
+    const host = this.closest("geocam-viewer");
+    if (!host) {
       console.error(
         "GeocamViewerOrbitControls must be a child of GeocamViewer"
       );
+      return;
     }
+
+    const attach = () => {
+      const viewer = host.viewer;
+      if (viewer && typeof viewer.plugin === "function") {
+        if (this.plugin) return;
+        this.viewer = viewer;
+        this.plugin = new orbitControls();
+        this.viewer.plugin(this.plugin);
+      } else {
+        setTimeout(attach, 50);
+      }
+    };
+
+    attach();
   }
 
   disconnectedCallback() {
-    this.controls = null;
+    if (this.plugin && typeof this.plugin.destroy === "function") {
+      this.plugin.destroy();
+    }
+    this.plugin = null;
     this.viewer = null;
     console.log("orbit controls disconnected");
     // Clean up the viewer
